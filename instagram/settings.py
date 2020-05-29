0c185e9cdb4ffe69f0fe01d54d3aa65ccc962a61
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-29 12:30:39 +0300

    create registration form

--- instagram/settings.py
+++ instagram/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social',
+    'bootstrap3',
+    'django-registration',
 ]
 
 MIDDLEWARE = [
