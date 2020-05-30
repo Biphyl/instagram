96965a06ea7c0a18d28bf2c8fe514a0c6e8035bb
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-30 08:04:09 +0300

    create static folder and forms filr

--- instagram/settings.py
+++ instagram/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'social',
     'bootstrap3',
     'django_registration',
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'biron',
+    'PASSWORD':'Biron4745',
     }
 }
 
@@ -109,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
