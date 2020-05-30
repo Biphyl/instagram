b4030d2afb736d26cb4f0fd1eb254de68702961f
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-30 08:36:19 +0300

    create comments model

--- social/models.py
+++ social/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
@@ -28,3 +30,20 @@ class Image(models.Model):
         
         return self.image_name
 
+class Comments(models.Model):
+    comment = models.CharField(max_length=100, blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = cls.objects.filter(image_id=id)
+        return comments
+
+    def save_comment(self):
+        self.save()
+
+    def __str__(self):
+
+        return self.comment
