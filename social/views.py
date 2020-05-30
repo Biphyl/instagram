eb9f7c270c98404b3fb9a2950665b60c58ab1d54
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-30 10:00:14 +0300

    create upload page

--- social/views.py
+++ social/views.py
@@ -10,3 +10,17 @@ def home(request):
 
     return render(request, 'index.html',{"images":images})
     
+def image_upload(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST,request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+        return redirect('home')
+
+    else:
+        form = ImageUploadForm()
+        return render(request,'upload.html', {"form":form})
+
