10674bf14775ae61505ce41cb6aa322dfe699628
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-30 11:24:42 +0300

    add social urls

--- social/urls.py
+++ social/urls.py
@@ -5,4 +5,11 @@ from django.conf import settings
 
 urlpatterns = [
     path(r'',views.home,name='home'),
+    path(r'images/', views.image_upload,name='upload'),
+    path(r'profile/', views.profile_info,name='profile'),
+    path(r'edit/', views.profile_edit,name='edit'),
+    path(r'new_comment/(\d+)/', views.add_commment,name='newComment'),
+    path(r'comment/', views.comments,name='comments'),
+    path(r'likes/(\d+)/', views.like_images,name='likes'),
+    path(r'user/',views.search_user,name='search_user')
 ]
