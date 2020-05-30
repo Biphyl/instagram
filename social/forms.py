d4ce992a8bd4c3b97a364806baa64e4c8fa188e6
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-30 09:17:41 +0300

    create ImageUpload,ImageProfile and Comment forms

---
+++ social/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Image
+
+class ImageUploadForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['profile','user','likes','comments']
+
+class ImageProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['user','image','date_posted']
