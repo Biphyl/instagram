c649fd4bef0e8fa71e7e3b273afbfc2df7c1d5ce
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-06-02 09:42:27 +0300

    fix errors

--- social/urls.py
+++ social/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -8,8 +9,8 @@ urlpatterns = [
     path(r'images/', views.image_upload,name='upload'),
     path(r'profile/', views.profile_info,name='profile'),
     path(r'edit/', views.profile_edit,name='edit'),
-    path(r'new_comment/(\d+)/', views.add_commment,name='newComment'),
+    path(r'add_comment/(\d+)/', views.add_commment,name='add_comment'),
     path(r'comment/', views.comments,name='comments'),
-    path(r'likes/(\d+)/', views.like_images,name='likes'),
+    path(r'like_image/(\d+)/', views.like_images,name='like_image'),
     path(r'user/',views.search_user,name='search_user')
 ]
